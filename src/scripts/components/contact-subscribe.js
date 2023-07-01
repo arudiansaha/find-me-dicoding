8817e24453e28fcbfb9874f13d87f24b395c210d
Rizky Ardiansyah <rizkyardiansyah@disroot.org>
2023-07-01 09:49:44 +0700

    Add newslatter subcription and copyright.

---
+++ src/scripts/components/contact-subscribe.js
@@ -0,0 +1,19 @@
+class ContactSubscribe extends HTMLElement {
+  connectedCallback() {
+    this._renderPage();
+  }
+
+  _renderPage() {
+    this.setAttribute('class', 'contact');
+    this.innerHTML = `
+      <h3 class="contact__title">Subscribe to our newsletter</h3>
+      <form class="contact__form">
+        <input class="contact__input" type="email" placeholder="your@mail.com" required />
+        <button class="contact__button" type="submit">Subscribe</button>
+      </form>
+      <p class="contact__copyright">&copy; 2023 Find Me</p>
+    `;
+  }
+}
+
+customElements.define('contact-subscribe', ContactSubscribe);
