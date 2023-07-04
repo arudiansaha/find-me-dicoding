180292c1c79f3633e3ec39b391cb59090a6d2370
Rizky Ardiansyah <rizkyardiansyah@disroot.org>
2023-07-04 08:59:47 +0700

    Rename private `_submitReview` into `_submitData`.

--- src/scripts/utils/submit-review.js
+++ src/scripts/utils/submit-review.js
@@ -2,16 +2,14 @@ import RestaurantApi from '../data/restaurant-api';
 
 const SubmitReview = {
   init({ form, name, review }) {
-    form.addEventListener('submit', (event) => {
-      event.preventDefault();
-      this._submitReview(name, review);
+    form.addEventListener('submit', () => {
+      this._submitData(name, review);
     });
   },
 
-  async _submitReview(name, review) {
+  async _submitData(name, review) {
     const id = window.location.hash.slice(9);
     const data = { id, name: name.value, review: review.value };
-
     await RestaurantApi.postRestaurantReview(data);
   },
 };
