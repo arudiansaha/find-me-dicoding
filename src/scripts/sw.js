eac411afddeb257bb3fa82c9beadfa98775e7b9f
Rizky Ardiansyah <rizkyardiansyah@disroot.org>
2023-07-20 09:22:39 +0700

    Implement stale while revalidate.

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,6 +1,21 @@
 import { precacheAndRoute } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate } from 'workbox-strategies';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import CONFIG from './globals/config';
 
 precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+  ({ url }) => url.href.startsWith(CONFIG.BASE_URL),
+  new StaleWhileRevalidate({
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }),
+);
 
 self.addEventListener('install', () => {
   console.log('Service Worker: Installed!');
